48f89798437f9f79fb8e2e8cb8893887dfee156d
abrehamashebir <abrisha96@gmail.com>
2024-08-31 21:47:58 +0300

    Extracting daily, weekly and anual sentiments

---
+++ streamlit_app/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="My Streamlit App",
+    page_icon=":chart_with_upwards_trend:",
+    layout="wide"
+)
+
+st.title("News Data Sentiment Analysis App")
+st.write('Time series analysis for news sentiment')
